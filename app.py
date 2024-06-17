f4033bfe66440bd90677e8e7e524bd715891bb67
Victormarsh001 <victorbasy17.3@gmail.com>
2024-06-17 19:26:50 +0000

    Before Nav bar
    Before Nav

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+JOBS = [{'id':1,
+         'job':'Backend Engineer',
+         'location': 'Lagos, Nigeria',
+         'salary':'$100,000'
+        },
+        {
+          'id':2,
+         'job':'Frontend Engineer',
+         'location': 'Delhi, India',
+         'salary':'$70,000'
+        },
+        {
+          'id':3,
+           'job':'DevOps',
+           'location': 'California, USA',
+           'salary':'$120,000'
+        },
+        {
+          'id':4,
+           'job':'ML Engineer',
+           'location': 'Abuja, Nigeria',
+           'salary':'$150,000'
+        },
+        {
+          'id':5,
+           'job':'Software Developer',
+           'location': 'Cross River, Nigeria',
+           'salary':'$170,000'
+        }
+
+  ]
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
   
